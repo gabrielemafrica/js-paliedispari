6105e0e270cb5ae391c3931443ae9053a0d1b22e
gabrielemafrica <gabriele.ga@live.it>
2023-05-08 21:35:54 +0300

    palindroma fine

--- palindroma/js/myscript.js
+++ palindroma/js/myscript.js
@@ -3,32 +3,82 @@
 
 //chiedi una parola
 
-let parola = prompt("inserisci una parola").toLowerCase();
-console.log(parola);
+//let parola = prompt("inserisci una parola").toLowerCase();
+// console.log(parola);
+
+//prendo il bottone
+const button = document.getElementById('verifica');
+
+//al click
+
+button.addEventListener(
+    'click',
+    function() {
+
+        //chiedi una parola
+        const parola = document.getElementById('parola').value.toLowerCase();
+        console.log(parola);
+        
+        //parola al contrario
+        const parolaContrario = parolaReverse(parola);
+        console.log(parolaContrario);
+
+        //risposta
+        let palindroma = false;
+
+        //valuto le parole
+        if (parola === parolaContrario) {
+            palindroma = true;
+        }
+
+        //do la risposta
+
+        let risposta;
+
+        //inserisco la risposta
+
+        if (palindroma) {
+
+            //inserisco la risposta
+            risposta = `<h2>La parola inserita è PALINDROMA</h2>`;         
+            console.log("é una parola PALINDROMA");
+        }else{
+            risposta  = `<h2>La parola inserita non é PALINDROMA</h2>`;     
+            console.log("non è una parola PALINDROMA");
+        }
+
+        //inserisco
+        const container = document.querySelector('div.risposta');
+        container.innerHTML = risposta;
+
+    }
+
+)
 
 //parola al contrario
-let parolaContrario = parolaReverse(parola);
-console.log(parolaContrario);
+// let parolaContrario = parolaReverse(parola);
+// console.log(parolaContrario);
 
 //risposta
+// let palindroma = false;
 
-let palindroma = false;
+
 //valuto le parole
 
-if (parola === parolaContrario) {
-    palindroma = true;
-}
+// if (parola === parolaContrario) {
+//     palindroma = true;
+// }
 
-console.log(palindroma);
+// console.log(palindroma);
 
 
 //do la risposta
 
-if (palindroma) {
-    console.log("é una parola PALINDROMA");
-}else{
-    console.log("non è una parola PALINDROMA");
-}
+// if (palindroma) {
+//     console.log("é una parola PALINDROMA");
+// }else{
+//     console.log("non è una parola PALINDROMA");
+// }
 
 
 
